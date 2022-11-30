756067a3a5cea7bd3708987a97c58c12d6f31abb
Seb Gertz <sebgertz@gmail.com>
2022-11-30 12:15:37 -0500

    feat: add page transitions

---
+++ src/components/Transition.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { TransitionGroup, Transition as ReactTransition } from 'react-transition-group';
+// The duration for each phase of the transition
+// So the total duration will be _twice_ this
+const timeout = 250;
+
+const defaultStyle = {
+  transition: `opacity ${timeout}ms ease-in-out`,
+  opacity: 0,
+}
+
+const transitionStyles = {
+  entering: { opacity: 0.8 },
+  entered:  { opacity: 1 },
+  exiting:  { opacity: 0.5 },
+  exited:  { opacity: 0 },
+};
+
+const Transition = ({ children, location }) => (
+  <TransitionGroup>
+    <ReactTransition key={location.pathname} timeout={timeout}>
+      {(status) => (
+        <div style={
+          {
+            ...defaultStyle,
+            ...transitionStyles[status]
+          }
+        }>
+          {children}
+        </div>
+      )}
+    </ReactTransition>
+  </TransitionGroup>
+);
+
+export default Transition;
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,16 +3,20 @@ import DrawerAppBar from "../components/navbar"
 import Footer from './footer'
 import { Stack } from '@mui/material';
 import Divider from '@mui/material/Divider';
+import Transition from '../components/Transition';
 
-const Layout = ({ children }) => {
+
+const Layout = ( { children, location }) => {
   return (
-    <Stack
-      divider={<Divider orientation="horizontal" flexItem />}
-      spacing={4}>
+      <Stack
+        divider={<Divider orientation="horizontal" flexItem />}
+        spacing={4}>
         <DrawerAppBar />
-        {children}
+        <Transition location={location}>
+          {children}
+        </Transition>
         <Footer/>
-    </Stack>
+      </Stack>
   )
 }
 

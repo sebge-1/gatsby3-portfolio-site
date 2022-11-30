d051fbb86fe766643c2d410432792554cd6c84a2
Seb Gertz <sebgertz@gmail.com>
2022-11-30 16:02:14 -0500

    feat: create category pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,25 +3,45 @@ const path = require('path')
 module.exports.createPages = async ({graphql, actions }) => {
   const {createPage} = actions;
   const blogTemplate = path.resolve('./src/templates/blogPost.js');
+  const categoryTemplate = path.resolve('./src/templates/categoryPage.js');
+
   const res = await graphql(`
     query {
       allContentfulBlogPost {
         edges {
           node {
             slug
+            tag
           }
         }
       }
     }
   `)
 
-  res.data.allContentfulBlogPost.edges.forEach((edge) => {
-    createPage({
-      component: blogTemplate,
-      path: `/blog/${edge.node.slug}`,
-      context: {
-        slug: edge.node.slug
-      }
-    })
-  })
+  const tags = new Set();
+
+  res.data.allContentfulBlogPost.edges
+    .forEach((edge) => {
+      createPage(
+        {
+          component: blogTemplate,
+          path: `/blog/${edge.node.slug}`,
+          context: {
+            slug: edge.node.slug
+          }
+        }
+      );
+      edge.node.tag.forEach(tag => tags.add(tag))
+    });
+  
+  tags.forEach((tag) => {
+    createPage(
+      {
+        component: categoryTemplate,
+        path: `/blog/tags/${tag}`,
+        context: {
+          tag: tag
+        }
+      });
+  }) 
 }

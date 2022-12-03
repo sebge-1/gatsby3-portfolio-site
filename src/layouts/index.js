519fec025438502acb5b3eeb6c5f26f8cc2d2a2b
Seb Gertz <sebgertz@gmail.com>
2022-12-02 23:12:06 -0500

    feat: enable dark mode toggle in Layout

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,23 +1,38 @@
-import React from "react"
-import DrawerAppBar from "../components/navbar"
-import Footer from './footer'
-import { Stack } from '@mui/material';
-import Divider from '@mui/material/Divider';
-import Transition from '../components/Transition';
+import { useState } from "react";
+import React from "react";
+import DrawerAppBar from "../components/navbar";
+import Footer from "./footer";
+import { Stack } from "@mui/material";
+import Divider from "@mui/material/Divider";
+import Transition from "../components/Transition";
+import { createTheme } from "@mui/material/styles";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import dark from "../themes/dark.js";
+import light from "../themes/light.js";
 
+const Layout = ({ children, location }) => {
+  const [darkMode, setDarkMode] = useState(false);
+  const activeTheme = createTheme(darkMode ? light : dark);
 
-const Layout = ( { children, location }) => {
   return (
+    <ThemeProvider theme={activeTheme}>
+      <CssBaseline enableColorScheme />
       <Stack
         divider={<Divider orientation="horizontal" flexItem />}
-        spacing={4}>
-        <DrawerAppBar />
-        <Transition location={location}>
-          {children}
-        </Transition>
-        <Footer/>
+        spacing={4}
+      >
+        <DrawerAppBar
+          color="secondary"
+          enableColorOnDark
+          darkModeActive={darkMode}
+          themeSetter={setDarkMode}
+        />
+        <Transition location={location}>{children}</Transition>
+        <Footer />
       </Stack>
-  )
-}
+    </ThemeProvider>
+  );
+};
 
-export default Layout
+export default Layout;

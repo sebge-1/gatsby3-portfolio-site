eab00f999a3f0c7a54e611ee0052a45d14e7fa47
Seb Gertz <sebgertz@gmail.com>
2022-12-02 23:10:36 -0500

    style: add light and dark themes

---
+++ src/themes/dark.js
@@ -0,0 +1,16 @@
+module.exports = {
+  palette: {
+    mode: "dark",
+
+    primary: {
+      main: "#90caf9",
+    },
+    secondary: {
+      main: "#f48fb1",
+    },
+    background: {
+      default: "#212121",
+      paper: "#424242",
+    },
+  },
+};
---
+++ src/themes/light.js
@@ -0,0 +1,13 @@
+module.exports = {
+  mode: "light",
+  primary: {
+    main: "#1976d2",
+  },
+  secondary: {
+    main: "rgb(220, 0, 78)",
+  },
+  background: {
+    default: "#fff",
+    paper: "#fff",
+  },
+};

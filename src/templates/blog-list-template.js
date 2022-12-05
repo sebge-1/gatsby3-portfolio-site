c2b46ddacaec08ab7ca16735861775085f3d8f1b
Seb Gertz <sebgertz@gmail.com>
2022-12-04 21:59:55 -0500

    refactor: update template imports/styling

--- src/templates/blog-list-template.js
+++ src/templates/blog-list-template.js
@@ -1,39 +1,44 @@
 import React from "react";
 import { Link } from "gatsby";
 import BlogList from "../components/BlogList";
-import { Container, Tab, Tabs } from "@mui/material";
+import { Container, Grid, Tab, Tabs } from "@mui/material";
+import SideBar from "../components/SideBar";
 
 export default function BlogListTemplate({ pageContext }) {
-  const { pageCount, group, index } = pageContext;
+  const { pageCount, group, index, tags } = pageContext;
   const previousUrl =
     index - 1 === 1 ? "/blog" : `/blog/${(index - 1).toString()}`;
   const nextUrl = `/blog/${(index + 1).toString()}`;
 
   return (
-    <Container>
-      <h1>Read articles from {pageCount} pages </h1>;
-      <BlogList posts={group} />
-      <Tabs sx={{ margin: 4 }} value={false}>
-        {index > 1 && (
-          <Tab to={previousUrl} label="Previous" component={Link}></Tab>
-        )}
-        {Array.from({ length: pageCount }, (_, idx) => {
-          // if (index === idx + 1) {
-
-          // }
-          return (
-            <Tab
-              key={idx}
-              label={idx + 1}
-              to={idx === 0 ? "/blog" : "/blog/" + (idx + 1)}
-              component={Link}
-            ></Tab>
-          );
-        })}
-        {index < pageCount && (
-          <Tab to={nextUrl} label="Next" component={Link}></Tab>
-        )}
-      </Tabs>
-    </Container>
+    <Grid container>
+      <Grid item lg={10} md={9} xs={8}>
+        <Container>
+          <h1>Read articles from {pageCount} pages </h1>
+          <BlogList posts={group} />
+          <Tabs sx={{ margin: 4 }} value={false}>
+            {index > 1 && (
+              <Tab to={previousUrl} label="Previous" component={Link}></Tab>
+            )}
+            {Array.from({ length: pageCount }, (_, idx) => {
+              return (
+                <Tab
+                  key={idx}
+                  label={idx + 1}
+                  to={idx === 0 ? "/blog" : "/blog/" + (idx + 1)}
+                  component={Link}
+                ></Tab>
+              );
+            })}
+            {index < pageCount && (
+              <Tab to={nextUrl} label="Next" component={Link}></Tab>
+            )}
+          </Tabs>
+        </Container>
+      </Grid>
+      <Grid item lg={2} md={3} xs={4}>
+        <SideBar tags={tags} />
+      </Grid>
+    </Grid>
   );
 }
--- src/templates/categoryPage.js
+++ src/templates/categoryPage.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Link } from "gatsby";
 import BlogList from "../components/BlogList";
-import { Container, Tab, Tabs } from "@mui/material";
+import { Container, Grid, Tab, Tabs } from "@mui/material";
+import SideBar from "../components/SideBar";
 
 export default function CategoryListTemplate({ pageContext }) {
-  const { pageCount, group, index } = pageContext;
-  const tag = pageContext.tag;
+  const { pageCount, group, index, tag, tags } = pageContext;
 
   const previousUrl =
     index - 1 === 1
@@ -14,31 +14,38 @@ export default function CategoryListTemplate({ pageContext }) {
   const nextUrl = `/blog/tags/${tag}/${(index + 1).toString()}`;
 
   return (
-    <Container>
-      <h1>Read articles from {pageCount} pages </h1>
-      <BlogList posts={group} />
-      <Tabs sx={{ margin: 4 }} value={false}>
-        {index > 1 && (
-          <Tab to={previousUrl} label="Previous" component={Link}></Tab>
-        )}
-        {Array.from({ length: pageCount }, (_, idx) => {
-          return (
-            <Tab
-              key={idx}
-              label={idx + 1}
-              to={
-                idx === 0
-                  ? `/blog/tags/${tag}`
-                  : `/blog/tags/${tag}/` + (idx + 1)
-              }
-              component={Link}
-            ></Tab>
-          );
-        })}
-        {index < pageCount && (
-          <Tab to={nextUrl} label="Next" component={Link}></Tab>
-        )}
-      </Tabs>
-    </Container>
+    <Grid container>
+      <Grid item lg={10} md={9} xs={8}>
+        <Container>
+          <h1>Read articles from {pageCount} pages </h1>
+          <BlogList posts={group} />
+          <Tabs sx={{ margin: 4 }} value={false}>
+            {index > 1 && (
+              <Tab to={previousUrl} label="Previous" component={Link}></Tab>
+            )}
+            {Array.from({ length: pageCount }, (_, idx) => {
+              return (
+                <Tab
+                  key={idx}
+                  label={idx + 1}
+                  to={
+                    idx === 0
+                      ? `/blog/tags/${tag}`
+                      : `/blog/tags/${tag}/` + (idx + 1)
+                  }
+                  component={Link}
+                ></Tab>
+              );
+            })}
+            {index < pageCount && (
+              <Tab to={nextUrl} label="Next" component={Link}></Tab>
+            )}
+          </Tabs>
+        </Container>
+      </Grid>
+      <Grid item lg={2} md={3} xs={4}>
+        <SideBar tags={tags} />
+      </Grid>
+    </Grid>
   );
 }

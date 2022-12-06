6cd846c701a593698bbf735b1b759d5dbf4ecd11
Seb Gertz <sebgertz@gmail.com>
2022-12-05 22:26:59 -0500

    style: wrap tag label in badge

--- src/components/TagList.js
+++ src/components/TagList.js
@@ -2,6 +2,7 @@ import * as React from "react";
 import Paper from "@mui/material/Paper";
 import MenuList from "@mui/material/MenuList";
 import MenuItem from "@mui/material/MenuItem";
+import Badge from "@mui/material/Badge";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import Chip from "@mui/material/Chip";
 import LabelIcon from "@mui/icons-material/Label";
@@ -12,15 +13,17 @@ export default function IconMenu(props) {
     <Paper sx={{ width: 320, maxWidth: "100%" }}>
       <MenuList>
         {props.tags.map((tag, index) => (
-          <MenuItem>
-            <ListItemIcon>
-              <Link to={`/blog/tags/${tag}`} key={index}>
-                <Chip
-                  key={index}
-                  label={tag}
-                  clickable
-                  icon={<LabelIcon />}
-                ></Chip>
+          <MenuItem key={index}>
+            <ListItemIcon key={index}>
+              <Link to={`/blog/tags/${tag.tagName}`} key={index}>
+                <Badge badgeContent={tag.postCount} color="secondary">
+                  <Chip
+                    key={index}
+                    label={tag.tagName}
+                    clickable
+                    icon={<LabelIcon />}
+                  />
+                </Badge>
               </Link>
             </ListItemIcon>
           </MenuItem>

a1df44561809eb6bd230fef52964ea1158027b43
Seb Gertz <sebgertz@gmail.com>
2022-12-09 08:12:12 -0500

    feat: add TOC component

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,10 +1,11 @@
 import React from "react";
 import TagList from "./TagList";
 import { Box } from "@mui/material";
+import TableOfContents from "./TableOfContents";
 
 const SideBar = (props) => (
   <Box sx={{ position: "fixed" }}>
-    <h1>tags</h1>
+    {props.sections && <TableOfContents sections={props.sections} />}
     <TagList tags={props.tags} />
   </Box>
 );

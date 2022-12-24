1a495d4aee47866928b89e2e9542fb5a0fa3939b
Seb Gertz <sebgertz@gmail.com>
2022-12-23 23:32:57 -0500

    refactor: add static query to layout

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import React from "react";
-import DrawerAppBar from "../components/navbar";
+import DrawerAppBar from "../components/nav/navbar";
 import Footer from "./footer";
 import { Stack } from "@mui/material";
 import Divider from "@mui/material/Divider";
@@ -10,10 +10,26 @@ import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import dark from "../themes/dark.js";
 import light from "../themes/light.js";
+import { useStaticQuery, graphql, Link } from "gatsby";
 
 const Layout = ({ children, location }) => {
   const [darkMode, setDarkMode] = useState(false);
   const activeTheme = createTheme(darkMode ? light : dark);
+
+  const postData = useStaticQuery(
+    graphql`
+      {
+        allContentfulBlogPost(sort: { publishedDate: DESC }) {
+          edges {
+            node {
+              title
+              slug
+            }
+          }
+        }
+      }
+    `
+  );
 
   return (
     <ThemeProvider theme={activeTheme}>
@@ -26,6 +42,7 @@ const Layout = ({ children, location }) => {
           enableColorOnDark
           darkModeActive={darkMode}
           themeSetter={setDarkMode}
+          postData={postData}
         />
         <Transition location={location}>{children}</Transition>
         <Footer />

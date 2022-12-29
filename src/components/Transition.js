f5758126a9a0dd418aa73ce9afbb88a1f5bfa603
Seb Gertz <sebgertz@gmail.com>
2022-12-29 17:57:00 -0500

    style: slow down page transition

--- src/components/Transition.js
+++ src/components/Transition.js
@@ -1,31 +1,34 @@
-import React from 'react';
-import { TransitionGroup, Transition as ReactTransition } from 'react-transition-group';
+import React from "react";
+import {
+  TransitionGroup,
+  Transition as ReactTransition,
+} from "react-transition-group";
 // The duration for each phase of the transition
 // So the total duration will be _twice_ this
-const timeout = 250;
+const timeout = 350;
 
 const defaultStyle = {
   transition: `opacity ${timeout}ms ease-in-out`,
   opacity: 0,
-}
+};
 
 const transitionStyles = {
   entering: { opacity: 0.8 },
-  entered:  { opacity: 1 },
-  exiting:  { opacity: 0.5 },
-  exited:  { opacity: 0 },
+  entered: { opacity: 1 },
+  exiting: { opacity: 0.5 },
+  exited: { opacity: 0 },
 };
 
 const Transition = ({ children, location }) => (
   <TransitionGroup>
     <ReactTransition key={location.pathname} timeout={timeout}>
       {(status) => (
-        <div style={
-          {
+        <div
+          style={{
             ...defaultStyle,
-            ...transitionStyles[status]
-          }
-        }>
+            ...transitionStyles[status],
+          }}
+        >
           {children}
         </div>
       )}

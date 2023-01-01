cb838de570bd7b5a1f22e6369dcbe58ac29311c3
Seb Gertz <sebgertz@gmail.com>
2022-12-31 23:07:55 -0500

    feat: add netflify form fields

--- src/components/nav/ContactForm.js
+++ src/components/nav/ContactForm.js
@@ -23,72 +23,86 @@ function ContactForm() {
     <div>
       <h2>Contact Form</h2>
       <Box sx={{ flexGrow: 1, mx: "2%" }}>
-        <Grid container spacing={2}>
-          <Grid item xs={6}>
-            <TextField
-              onChange={(e) => setFirstName(e.target.value)}
-              value={firstName}
-              style={{ width: "100%" }}
-              id="outlined-error-helper-text"
-              label="First name"
-              placeholder="Enter first name..."
-            />
+        <form
+          id="contact-form"
+          name="contact-form"
+          method="POST"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+        >
+          <input type="hidden" name="form-name" value="contact-form" />
+          <Grid container spacing={2}>
+            <Grid item xs={6}>
+              <TextField
+                onChange={(e) => setFirstName(e.target.value)}
+                value={firstName}
+                style={{ width: "100%" }}
+                id="outlined-error-helper-text"
+                label="First name"
+                placeholder="Enter first name..."
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                onChange={(e) => setLastName(e.target.value)}
+                value={lastName}
+                style={{ width: "100%" }}
+                id="outlined-error-helper-text"
+                label="Last name"
+                placeholder="Enter last name..."
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                style={{ width: "100%" }}
+                id="outlined-error-helper-text"
+                label="Email"
+                placeholder="Enter contact email..."
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                onChange={(e) => setSubject(e.target.value)}
+                value={subject}
+                style={{ width: "100%" }}
+                id="outlined-error-helper-text"
+                label="Subject"
+                placeholder="Enter subject regards..."
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                onChange={(e) => setContent(e.target.value)}
+                value={content}
+                style={{ width: "100%" }}
+                placeholder="Enter reason for contact"
+                multiline
+                rows={4}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Button style={{ width: "100%" }} variant="contained">
+                Send Message
+              </Button>
+              <Snackbar
+                open={open}
+                autoHideDuration={3000}
+                anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+              >
+                <Alert severity="success" sx={{ width: "100%" }}>
+                  Message Sent!
+                </Alert>
+              </Snackbar>
+            </Grid>
+            <p hidden>
+              <label>
+                Don’t fill this out: <input name="bot-field" />
+              </label>
+            </p>
           </Grid>
-          <Grid item xs={6}>
-            <TextField
-              onChange={(e) => setLastName(e.target.value)}
-              value={lastName}
-              style={{ width: "100%" }}
-              id="outlined-error-helper-text"
-              label="Last name"
-              placeholder="Enter last name..."
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              onChange={(e) => setEmail(e.target.value)}
-              value={email}
-              style={{ width: "100%" }}
-              id="outlined-error-helper-text"
-              label="Email"
-              placeholder="Enter contact email..."
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              onChange={(e) => setSubject(e.target.value)}
-              value={subject}
-              style={{ width: "100%" }}
-              id="outlined-error-helper-text"
-              label="Subject"
-              placeholder="Enter subject regards..."
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              onChange={(e) => setContent(e.target.value)}
-              value={content}
-              style={{ width: "100%" }}
-              placeholder="Enter reason for contact"
-              multiline
-              rows={4}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <Button style={{ width: "100%" }} variant="contained">
-              Send Message
-            </Button>
-            <Snackbar
-              open={open}
-              autoHideDuration={3000}
-              anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
-            >
-              <Alert severity="success" sx={{ width: "100%" }}>
-                Message Sent!
-              </Alert>
-            </Snackbar>
-          </Grid>
-        </Grid>
+        </form>
       </Box>
     </div>
   );

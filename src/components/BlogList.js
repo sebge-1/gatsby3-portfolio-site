c54d6a82aab45beeed5f270d5d1f1f29205f6852
Seb Gertz <sebgertz@gmail.com>
2023-01-07 22:25:28 -0500

    style: change blog list layout

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,82 +1,117 @@
 import React, { useState } from "react";
 import { GatsbyImage } from "gatsby-plugin-image";
 import { Link } from "gatsby";
-import { Grid, Card, Typography } from "@mui/material";
+import { Card, Typography, Stack, Box } from "@mui/material";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Chip from "@mui/material/Chip";
 import { slugify } from "../utils/slugify";
-import { Container } from "@mui/material";
 
 const BlogList = (props) => {
   return (
-    <Grid
-      container
+    <Stack
       spacing={{ xs: 2, md: 3 }}
-      columns={{ xs: 4, sm: 8, md: 12 }}
+      justifyContent="center"
+      alignItems="center"
+      direction="column"
     >
       {props.posts.map((edge, index) => {
         return (
-          <Grid item sm={6} md={4} key={index}>
-            <Card sx={{ height: "100%", maxWidth: "345px" }} key={index}>
-              <Link to={`/blog/${edge.node.slug}`}>
-                <CardMedia>
-                  {edge.node.heroImage && (
-                    <GatsbyImage
-                      image={edge.node.heroImage.gatsbyImageData}
-                      alt={edge.node.heroImage.description}
-                    />
-                  )}
-                </CardMedia>
-              </Link>
+          <Card
+            key={index}
+            sx={{
+              display: "flex",
+              height: "100%",
+              width: {
+                lg: "70%",
+                md: "85%",
+                sm: "90%",
+                xs: "90%",
+              },
+            }}
+          >
+            <Box
+              key={index}
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+              }}
+            >
+              <Box
+                key={index}
+                sx={{ height: "100%" }}
+                display={{ xs: "none", sm: "block", md: "block" }}
+                component={CardMedia}
+              >
+                {edge.node.heroImage && (
+                  <GatsbyImage
+                    image={edge.node.heroImage.gatsbyImageData}
+                    alt={edge.node.heroImage.description}
+                    style={{
+                      width: "200px",
+                      height: "100%",
+                    }}
+                    imgStyle={{
+                      objectFit: "cover",
+                    }}
+                  />
+                )}
+              </Box>
               <CardContent>
-                <Typography variant="overline" color="text.secondary">
-                  {edge.node.publishedDate}
-                </Typography>
-                <Link to={`/blog/${edge.node.slug}`}>
-                  <Typography gutterBottom variant="h6" component="div">
-                    {edge.node.title}
+                <CardContent>
+                  <Typography variant="overline" color="text.secondary">
+                    {edge.node.publishedDate}
                   </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    {edge.node.tldr.internal.content}
-                  </Typography>
-                </Link>
-              </CardContent>
-
-              <CardContent>
-                {edge.node.tag &&
-                  edge.node.tag.map((tag, index) => {
-                    const slug = slugify(tag);
-                    return (
-                      <Link to={`/blog/tags/${slug}`} key={index}>
-                        <Chip
-                          key={index}
-                          label={tag}
-                          clickable
-                          onClick={() => props.setActiveTag(slug)}
-                          sx={{
-                            bgcolor:
-                              props.activeTag && props.activeTag === slug
-                                ? "#9E9EFF"
-                                : "",
-                            "&:hover": {
+                  <Link to={`/blog/${edge.node.slug}`}>
+                    <Typography gutterBottom variant="h6" component="div">
+                      {edge.node.title}
+                    </Typography>
+                    <Box display={{ sm: "none", md: "inline-block" }}>
+                      <Typography
+                        variant="body2"
+                        color="text.secondary"
+                        component="div"
+                      >
+                        {edge.node.tldr.internal.content}
+                      </Typography>
+                    </Box>
+                  </Link>
+                </CardContent>
+                <CardContent>
+                  {edge.node.tag &&
+                    edge.node.tag.map((tag, index) => {
+                      const slug = slugify(tag);
+                      return (
+                        <Link to={`/blog/tags/${slug}`} key={index}>
+                          <Chip
+                            key={index}
+                            label={tag}
+                            clickable
+                            onClick={() => props.setActiveTag(slug)}
+                            sx={{
                               bgcolor:
                                 props.activeTag && props.activeTag === slug
-                                  ? "#b8b8ff"
+                                  ? "#9E9EFF"
                                   : "",
-                            },
-                            mr: "5px",
-                          }}
-                        />
-                      </Link>
-                    );
-                  })}
+                              "&:hover": {
+                                bgcolor:
+                                  props.activeTag && props.activeTag === slug
+                                    ? "#b8b8ff"
+                                    : "",
+                              },
+                              mr: "5px",
+                            }}
+                          />
+                        </Link>
+                      );
+                    })}
+                </CardContent>
               </CardContent>
-            </Card>
-          </Grid>
+            </Box>
+          </Card>
         );
       })}
-    </Grid>
+    </Stack>
   );
 };
 

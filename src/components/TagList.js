1ce98715d99e5267a0e3c59f90543046ece2cdc2
Seb Gertz <sebgertz@gmail.com>
2023-01-07 22:26:23 -0500

    refactor: rm Box component from tag list

--- src/components/TagList.js
+++ src/components/TagList.js
@@ -3,40 +3,35 @@ import Badge from "@mui/material/Badge";
 import Chip from "@mui/material/Chip";
 import { Link } from "gatsby";
 import { slugify } from "../utils/slugify";
-import { Box } from "@mui/material";
 
 export default function TagList({ activeTag, setActiveTag, tags }) {
   return (
     <>
-      <h1>Tags</h1>
-      <Box>
-        {tags.map((tag, index) => (
-          <Badge badgeContent={tag.postCount} color="secondary" key={index}>
-            <Link key={index} to={`/blog/tags/${slugify(tag.tagName)}`}>
-              <Chip
-                key={index}
-                label={tag.tagName}
-                clickable
-                sx={{
+      {tags.map((tag, index) => (
+        <Badge badgeContent={tag.postCount} color="secondary" key={index}>
+          <Link key={index} to={`/blog/tags/${slugify(tag.tagName)}`}>
+            <Chip
+              key={index}
+              label={tag.tagName}
+              clickable
+              sx={{
+                bgcolor:
+                  activeTag && activeTag === slugify(tag.tagName)
+                    ? "#9E9EFF"
+                    : "",
+                "&:hover": {
                   bgcolor:
                     activeTag && activeTag === slugify(tag.tagName)
-                      ? "#9E9EFF"
+                      ? "#b8b8ff"
                       : "",
-                  "&:hover": {
-                    bgcolor:
-                      activeTag && activeTag === slugify(tag.tagName)
-                        ? "#b8b8ff"
-                        : "",
-                  },
-                  m: "2.5px",
-                  p: "5px",
-                }}
-                onClick={() => setActiveTag && setActiveTag(tag.tagName)}
-              />
-            </Link>
-          </Badge>
-        ))}
-      </Box>
+                },
+                m: "2.5px",
+              }}
+              onClick={() => setActiveTag && setActiveTag(tag.tagName)}
+            />
+          </Link>
+        </Badge>
+      ))}
     </>
   );
 }

c8884229dbff778992eaa50bc20bb51127ef88e2
Seb Gertz <sebgertz@gmail.com>
2023-01-07 22:21:53 -0500

    build: comment out dotenv refs

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,43 +2,53 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 
- const fs = require('fs');
- const dotenv = require('dotenv');
- const envConfig = 
- dotenv.parse(fs.readFileSync('.env'));
- for (var k in envConfig) {
-   process.env[k] = envConfig[k];
- }
+// const fs = require("fs");
+// const dotenv = require("dotenv");
+// const envConfig = dotenv.parse(fs.readFileSync(".env"));
+// for (var k in envConfig) {
+//   process.env[k] = envConfig[k];
+// }
 
 module.exports = {
   siteMetadata: {
     title: `Sebastian Gertz`,
     siteUrl: `https://www.sebastiangertz.com`,
-    description: 'Personal blog and portfolio site',
-    image: '👨🏻‍💻'
+    description: "Personal blog and portfolio site",
+    image: "👨🏻‍💻",
   },
-  plugins: [{
-    resolve: 'gatsby-source-contentful',
-    options: {
-      "accessToken": process.env.CONTENTFUL_ACCESS_TOKEN,
-      "spaceId": "vb0h3gv8lx2b"
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-layout", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-sass", {
-    resolve: 'gatsby-plugin-google-analytics',
-    options: {
-      "trackingId": "G-VWSTSMGH7S"
-    }
-  }, "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        spaceId: "vb0h3gv8lx2b",
+      },
     },
-    __key: "images"
-  }]
+    "gatsby-plugin-image",
+    "gatsby-plugin-layout",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sass",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "G-VWSTSMGH7S",
+      },
+    },
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
 };

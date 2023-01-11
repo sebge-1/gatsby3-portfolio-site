6070cfdb56b267f22d97ccbb24098f4c9c168d78
Seb Gertz <sebgertz@gmail.com>
2023-01-11 18:42:07 -0500

    style: tweak dark and light themes

--- src/themes/dark.js
+++ src/themes/dark.js
@@ -9,8 +9,17 @@ module.exports = {
       main: "#f48fb1",
     },
     background: {
-      default: "#212121",
-      paper: "#424242",
+      paper: "#100c08",
+    },
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          background:
+            "radial-gradient(circle at 24.1% 68.8%, rgb(50, 50, 50) 0%, rgb(0, 0, 0) 99.4%)",
+        },
+      },
     },
   },
 };
--- src/themes/light.js
+++ src/themes/light.js
@@ -1,13 +1,25 @@
 module.exports = {
-  mode: "light",
-  primary: {
-    main: "#1976d2",
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#4b177a",
+    },
+    secondary: {
+      main: "#f48fb1",
+    },
+    background: {
+      default: "#fff",
+      paper: "#FAF9F6",
+    },
   },
-  secondary: {
-    main: "rgb(220, 0, 78)",
-  },
-  background: {
-    default: "#fff",
-    paper: "#fff",
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          background:
+            "radial-gradient(circle, rgba(250,249,246,1) 13%, rgba(241,236,245,1) 66%, rgba(241,236,245,1) 76%, rgba(241,236,245,1) 87%, rgba(241,236,245,1) 94%);",
+        },
+      },
+    },
   },
 };

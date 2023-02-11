86d652e6f5736fc338dec7aabbd9a2d7fa3cb002
Seb Gertz <sebgertz@gmail.com>
2023-02-10 20:53:04 -0500

    style: change color scheme

--- src/themes/light.js
+++ src/themes/light.js
@@ -2,24 +2,14 @@ module.exports = {
   palette: {
     mode: "light",
     primary: {
-      main: "#4b177a",
+      main: "#3F51B5",
     },
     secondary: {
       main: "#f48fb1",
     },
     background: {
-      default: "#fff",
-      paper: "#FAF9F6",
-    },
-  },
-  components: {
-    MuiCssBaseline: {
-      styleOverrides: {
-        body: {
-          background:
-            "radial-gradient(circle, rgba(250,249,246,1) 13%, rgba(241,236,245,1) 66%, rgba(241,236,245,1) 76%, rgba(241,236,245,1) 87%, rgba(241,236,245,1) 94%);",
-        },
-      },
+      default: "#FBFBFB",
+      paper: "#FFFFFF",
     },
   },
 };

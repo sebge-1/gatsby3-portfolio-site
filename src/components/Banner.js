5e9a5e676a8a0eec949494c4e952feef0ac915db
Seb Gertz <sebgertz@gmail.com>
2023-02-13 11:44:18 -0500

    style: overhaul banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,35 +2,23 @@ import React from "react";
 import { Typography, Box } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 
-const style = {
-  width: "100%",
-  height: "25vh",
-};
-
-const darkStyle = {
-  background: "linear-gradient(to right bottom, #90caf9 50.3%, #212121 50% )",
-};
-
-const lightStyle = {
-  background: "linear-gradient(to right bottom, #3F51B5 50.3%, #FBFBFB 50% )",
-};
-
 const Banner = () => {
-  let activeTheme = useTheme();
-  console.log(activeTheme);
-  let bannerStyle =
-    activeTheme.palette.mode === "dark" ? darkStyle : lightStyle;
+  const activeTheme = useTheme();
+
   return (
     <Box
       className="responsive-diagonal"
       sx={{
         marginTop: "0.8rem",
         padding: "2rem",
-        ...style,
-        ...bannerStyle,
+        width: "100%",
+        height: "35vh",
+        background: `linear-gradient(to right bottom, ${activeTheme.palette.primary.main} 50.3%, ${activeTheme.palette.background.default} 50% )`,
       }}
     >
-      <Typography variant="h2">Blog</Typography>
+      <Typography variant="h2" color={"white"}>
+        Blog
+      </Typography>
     </Box>
   );
 };

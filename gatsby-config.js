adb410f49d4c7b426a2b5232390b9a0867f7a600
Seb Gertz <sebgertz@gmail.com>
2023-03-07 21:58:16 -0500

    bug fix: comment out dotenv for prod

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 
-const fs = require("fs");
-const dotenv = require("dotenv");
-const envConfig = dotenv.parse(fs.readFileSync(".env"));
-for (var k in envConfig) {
-  process.env[k] = envConfig[k];
-}
+// const fs = require("fs");
+// const dotenv = require("dotenv");
+// const envConfig = dotenv.parse(fs.readFileSync(".env"));
+// for (var k in envConfig) {
+//   process.env[k] = envConfig[k];
+// }
 
 module.exports = {
   siteMetadata: {

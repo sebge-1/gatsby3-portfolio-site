dd268fae8e84566f1763e33402110f699f1e2774
Seb Gertz <sebgertz@gmail.com>
2023-03-09 16:29:30 -0500

    style: add animations

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -7,6 +7,7 @@ import CardMedia from "@mui/material/CardMedia";
 import Chip from "@mui/material/Chip";
 import { slugify } from "../utils/slugify";
 import { useTheme } from "@mui/material/styles";
+import Slide from "@mui/material/Slide";
 
 const BlogList = (props) => {
   const activeTheme = useTheme();
@@ -20,117 +21,125 @@ const BlogList = (props) => {
     >
       {props.posts.map((edge, index) => {
         return (
-          <Card
+          <Slide
+            direction="up"
+            in={true}
+            mountOnEnter
+            unmountOnExit
             key={index}
-            sx={{
-              display: "flex",
-              height: "100%",
-              width: {
-                lg: "70%",
-                md: "85%",
-                sm: "90%",
-                xs: "90%",
-              },
-              ":hover": {
-                boxShadow: "10px 10px 5px 0px rgba(212,190,212,1)",
-              },
-            }}
-            raised={true}
           >
-            <Box
+            <Card
               key={index}
               sx={{
                 display: "flex",
-                flexDirection: "row",
+                height: "100%",
+                width: {
+                  lg: "70%",
+                  md: "85%",
+                  sm: "90%",
+                  xs: "90%",
+                },
+                ":hover": {
+                  boxShadow: "10px 10px 5px 0px rgba(212,190,212,1)",
+                },
               }}
+              raised={true}
             >
               <Box
                 key={index}
-                sx={{ height: "100%" }}
-                display={{ xs: "none", sm: "block", md: "block" }}
-                component={CardMedia}
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                }}
               >
-                {edge.node.heroImage && (
-                  <GatsbyImage
-                    image={edge.node.heroImage.gatsbyImageData}
-                    alt={edge.node.heroImage.description}
-                    style={{
-                      width: "150px",
-                      height: "100%",
-                    }}
-                    imgStyle={{
-                      objectFit: "cover",
-                    }}
-                  />
-                )}
-              </Box>
-              <CardContent>
+                <Box
+                  key={index}
+                  sx={{ height: "100%" }}
+                  display={{ xs: "none", sm: "block", md: "block" }}
+                  component={CardMedia}
+                >
+                  {edge.node.heroImage && (
+                    <GatsbyImage
+                      image={edge.node.heroImage.gatsbyImageData}
+                      alt={edge.node.heroImage.description}
+                      style={{
+                        width: "150px",
+                        height: "100%",
+                      }}
+                      imgStyle={{
+                        objectFit: "cover",
+                      }}
+                    />
+                  )}
+                </Box>
                 <CardContent>
-                  <Typography variant="overline" color="text.secondary">
-                    {edge.node.publishedDate}
-                  </Typography>
-                  <Link to={`/blog/${edge.node.slug}`}>
-                    <Typography gutterBottom variant="h6" component="div">
-                      {edge.node.title}
+                  <CardContent>
+                    <Typography variant="overline" color="text.secondary">
+                      {edge.node.publishedDate}
                     </Typography>
-                    <Box display={{ sm: "none", md: "inline-block" }}>
-                      <Typography
-                        variant="body2"
-                        color="text.secondary"
-                        component="div"
-                      >
-                        {edge.node.tldr.internal.content}
+                    <Link to={`/blog/${edge.node.slug}`}>
+                      <Typography gutterBottom variant="h6" component="div">
+                        {edge.node.title}
                       </Typography>
-                    </Box>
-                  </Link>
-                </CardContent>
-                <CardContent>
-                  {edge.node.tag &&
-                    edge.node.tag.map((tag, index) => {
-                      const slug = slugify(tag);
-                      return (
-                        <Link
-                          to={
-                            slug === props.activeTag
-                              ? `/blog`
-                              : `/blog/tags/${slug}`
-                          }
-                          key={index}
+                      <Box display={{ sm: "none", md: "inline-block" }}>
+                        <Typography
+                          variant="body2"
+                          color="text.secondary"
+                          component="div"
                         >
-                          <Chip
-                            key={index}
-                            label={tag}
-                            clickable
-                            onClick={() =>
-                              props.setActiveTag && props.setActiveTag(slug)
+                          {edge.node.tldr.internal.content}
+                        </Typography>
+                      </Box>
+                    </Link>
+                  </CardContent>
+                  <CardContent>
+                    {edge.node.tag &&
+                      edge.node.tag.map((tag, index) => {
+                        const slug = slugify(tag);
+                        return (
+                          <Link
+                            to={
+                              slug === props.activeTag
+                                ? `/blog`
+                                : `/blog/tags/${slug}`
                             }
-                            sx={{
-                              bgcolor:
-                                props.activeTag && props.activeTag === slug
-                                  ? `${activeTheme.palette.primary.main}`
-                                  : "",
-                              "&:hover": {
+                            key={index}
+                          >
+                            <Chip
+                              key={index}
+                              label={tag}
+                              clickable
+                              onClick={() =>
+                                props.setActiveTag && props.setActiveTag(slug)
+                              }
+                              sx={{
                                 bgcolor:
                                   props.activeTag && props.activeTag === slug
-                                    ? `${activeTheme.palette.primary.light}`
+                                    ? `${activeTheme.palette.primary.main}`
                                     : "",
-                              },
-                              "& .MuiChip-label": {
-                                color:
-                                  props.activeTag && props.activeTag === slug
-                                    ? `white`
-                                    : "",
-                              },
-                              mr: "5px",
-                            }}
-                          />
-                        </Link>
-                      );
-                    })}
+                                "&:hover": {
+                                  bgcolor:
+                                    props.activeTag && props.activeTag === slug
+                                      ? `${activeTheme.palette.primary.light}`
+                                      : "",
+                                },
+                                "& .MuiChip-label": {
+                                  color:
+                                    props.activeTag && props.activeTag === slug
+                                      ? `white`
+                                      : "",
+                                },
+                                mr: "5px",
+                              }}
+                            />
+                          </Link>
+                        );
+                      })}
+                  </CardContent>
                 </CardContent>
-              </CardContent>
-            </Box>
-          </Card>
+              </Box>
+            </Card>
+          </Slide>
         );
       })}
     </Stack>
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import { Link } from "gatsby";
 import { Container, Grid, Typography, Divider, Box } from "@mui/material";
 import BackgroundAnimation from "../components/BackgroundAnimation";
 import TypeWriter from "../components/TypeWriter";
+import { Zoom } from "@mui/material";
 
 function Home() {
   return (
@@ -18,76 +19,78 @@ function Home() {
         alignSelf: "center",
       }}
     >
-      <Grid
-        container
-        direction="column"
-        sx={{
-          padding: "5%",
-          alignSelf: "center",
-          maxWidth: "900px",
-          borderRadius: "1%",
-          boxShadow: "0 8px 18px rgba(144, 202, 249, 0.6)",
-          textAlign: "center",
-        }}
-      >
-        <BackgroundAnimation className="background-animation" />
+      <Zoom in={true}>
+        <Grid
+          container
+          direction="column"
+          sx={{
+            padding: "5%",
+            alignSelf: "center",
+            maxWidth: "900px",
+            borderRadius: "1%",
+            boxShadow: "0 8px 18px rgba(144, 202, 249, 0.6)",
+            textAlign: "center",
+          }}
+        >
+          <BackgroundAnimation className="background-animation" />
 
-        <Grid item xs={12} sm={9} md={9} lg={9} sx={{ padding: "5%" }}>
-          <Typography
-            sx={{
-              // typography: { xs: "h2", sm: "h1" },
-              fontSize: {
-                lg: 80,
-                md: 80,
-                sm: 60,
-                xs: 60,
-              },
-            }}
-            fontFamily="Montserrat"
-            variant="h1"
-          >
-            Hello there!
-          </Typography>
+          <Grid item xs={12} sm={9} md={9} lg={9} sx={{ padding: "5%" }}>
+            <Typography
+              sx={{
+                // typography: { xs: "h2", sm: "h1" },
+                fontSize: {
+                  lg: 80,
+                  md: 80,
+                  sm: 60,
+                  xs: 60,
+                },
+              }}
+              fontFamily="Montserrat"
+              variant="h1"
+            >
+              Hello there!
+            </Typography>
 
-          <Typography
-            className="wave"
-            sx={{ typography: { xs: "h2", sm: "h1" } }}
-          >
-            👋🏻
-          </Typography>
-          <Divider
-            sx={{
-              margin: "auto",
-              width: "15%",
-              maxWidth: "100px",
-              borderTop: "3px solid",
-            }}
-          ></Divider>
+            <Typography
+              className="wave"
+              sx={{ typography: { xs: "h2", sm: "h1" } }}
+            >
+              👋🏻
+            </Typography>
+            <Divider
+              sx={{
+                margin: "auto",
+                width: "15%",
+                maxWidth: "100px",
+                borderTop: "3px solid",
+              }}
+            ></Divider>
+          </Grid>
+          <Grid item xs={12} sx={{ margin: "0 auto" }}>
+            <Typography
+              variant="body2"
+              fontSize="1.00rem"
+              fontWeight="bold"
+              fontFamily="merriweather"
+              align="left"
+              // verticalAlign="middle"
+            >
+              This is my digital home. Have fun looking around! You can find
+              more about me{" "}
+              <Link style={{ fontFamily: "merriweather" }} to="/about">
+                here
+              </Link>
+              . Soon there'll be more content. Right now, there's a blog where I
+              write about topics I find interesting, for example{" "}
+              <TypeWriter
+                loop={true}
+                strings={["Gatsby.", "P5.js.", "coding challenges."]}
+                style={{ display: "inline-block" }}
+              />
+            </Typography>
+          </Grid>
         </Grid>
-        <Grid item xs={12} sx={{ margin: "0 auto" }}>
-          <Typography
-            variant="body2"
-            fontSize="1.00rem"
-            fontWeight="bold"
-            fontFamily="merriweather"
-            align="left"
-            // verticalAlign="middle"
-          >
-            This is my digital home. Have fun looking around! You can find more
-            about me{" "}
-            <Link style={{ fontFamily: "merriweather" }} to="/about">
-              here
-            </Link>
-            . Soon there'll be more content. Right now, there's a blog where I
-            write about topics I find interesting, for example{" "}
-            <TypeWriter
-              loop={true}
-              strings={["Gatsby.", "P5.js.", "coding challenges."]}
-              style={{ display: "inline-block" }}
-            />
-          </Typography>
-        </Grid>
-      </Grid>
+      </Zoom>
     </Container>
   );
 }
